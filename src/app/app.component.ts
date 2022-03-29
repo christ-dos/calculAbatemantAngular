c8d5bd10ad9fd912bb81ce3040f92d9309d6ddfc
Christine Duarte <christine_dos_santos@hotmail.fr>
2022-03-30 00:48:12 +0200

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,33 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Child } from './models/child.model';
+import { ChildService } from './services/child.service';
+import { Observable } from "rxjs";
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'calculabatementapp';
+export class AppComponent implements OnInit{
+ 
+  
+  public children!: Child[];
+
+  constructor(private childService: ChildService){}
+
+  ngOnInit(): void {
+       this.getChildren();
+  }
+
+  public getChildren(): void{
+    this.childService.getAllChildren().subscribe(
+      (response: Child[]) =>{
+        this.children = response;
+      }
+      // (error: HttpErrorResponse) =>{
+      //   alert(error.message);
+      // }
+    )
+  }
 }
